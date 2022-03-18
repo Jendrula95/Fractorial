2f63ee44495d6711cac56daf00f3a510ac59f589
Andrzej Kołacz <Jendrula95@hotmail.com>
2022-03-18 13:18:39 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Fractorial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+   
+        class Fractorial : IEquatable, IComparable<Fractorial>
+
+        {
+        // Uzywam Getterów i Setterów do wyświetlania tylko licznika i mianownika
+            protected int num { get; set; }
+            protected int nom { get; set; }
+
+            // tworze konstruktor domyślny
+
+        public Fractorial()
+            {
+            
+               
+        }
+    
+        // tworze konstruktor kopiujacy
+            public Fractorial(Fractorial previousFractorial)
+            {
+                num = previousFractorial.num;
+                nom = previousFractorial.nom;
+            }
+       
+           // tworze konstruktor z dwoma argumentami
+            public Fractorial(int numerator, int nominative)
+            {
+            if (nominative == 0)
+            {
+                throw new ArgumentException("nominative cannot be zero.", nameof(nominative));
+            }
+            num = numerator;
+            nom = nominative;
+            }
+        // tworze przeciążam operatorów +,-,/,*
+            public static Fractorial operator +(Fractorial a) => a;
+            public static Fractorial operator -(Fractorial a) => new Fractorial(-a.num, a.nom);
+
+            public static Fractorial operator +(Fractorial a, Fractorial b)
+                => new Fractorial(a.num * b.nom + b.num * a.nom, a.nom * b.nom);
+
+            public static Fractorial operator -(Fractorial a, Fractorial b)
+                => a + (-b);
+
+            public static Fractorial operator *(Fractorial a, Fractorial b)
+                => new Fractorial(a.num * b.num, a.nom * b.nom);
+            public static Fractorial operator /(Fractorial a, Fractorial b)
+            {
+                if (b.num == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                return new Fractorial(a.num * b.nom, a.nom * b.num);
+            }
+
+            public override string ToString() => $"{num} / {nom}";
+
+            public string ToString(string fr)
+            {
+                return this.ToString();
+            }
+        // tworze metode CompareTo do sprawdzenia poprawności IComparable poprzez pobranie dwóch ułamków i porównanie ich ze sobą
+        public int CompareTo(Fractorial other)
+        {
+            if(other ==null) return 1;
+            int check = (this.num * other.nom) - (other.num * this.nom );
+            if (check > 0) return 1;
+            else if (check == 0) return 0;
+            else return -1;
+        }
+        /* tworze metode RoundUp która jawnie zaokrągla ułamek w góre dzieląc licznik przez mianownik nastepnie odejmując od niego modulo z dzielenia licznika przez mianownik i 
+         sprawdzając czy jest większy od zera jeśli tak to do wyniku z modulo dodajemy 1 i w ten sposób zaokrąglamy w góre jeśli nie to wyświetla wynik z modulo*/
+        public int RoundUp()
+        {
+            double result = (double)this.num / (double)this.nom;
+            int result2 = this.num % this.nom;
+            if (result - (int)(result2) > 0)
+            {
+                return result2 + 1;
+            }
+            return result2;
+        }
+        // tworze metode RoundDown która zaokrągla w dół poprzez modulo z licznika i mianownika
+        public int RoundDown()
+        {
+            return this.num % this.nom;
+        }
+    }
+    }
+
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+      public  static void Main(string[] args)
+        {
+            var a = new Fractorial(5, 4);
+            var b = new Fractorial(1, 2);
+            Console.WriteLine(-a);   // output: -5 / 4
+            Console.WriteLine(a + b);  // output: 14 / 8
+            Console.WriteLine(a - b);  // output: 6 / 8
+            Console.WriteLine(a * b);  // output: 5 / 8
+            Console.WriteLine(a / b);  // output: 10 / 4
+            Console.WriteLine("-----------------------------------------------");
+            // wywołuje metody RoundUp oraz RoundDown
+            Console.WriteLine(a.RoundUp());
+            Console.WriteLine(a.RoundDown());
+            Console.WriteLine("----------------------------------------------");
+            // Sprawdzam za pomocą losowych 10 ułamków czy interfejs IComparable działa poprawnie losując najpierw ułamkie a następnie je sortuje
+            Fractorial[] fraction;
+            fraction = new Fractorial[10];
+            Random random = new Random(10);
+            for (int i = 0; i < 10; ++i)
+            {
+                fraction[i] = new Fractorial(random.Next(10), random.Next(10));
+                Console.WriteLine(fraction[i]);
+            }
+            Console.WriteLine("------------------------------------------------");
+            System.Array.Sort(fraction);
+
+            for (int i = 0; i < 10; ++i)
+            {
+                Console.WriteLine(fraction[i]);
+            }
+        }
+    }
+}
